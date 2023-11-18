ff8ce75f178994da90d35e34b54333434ce87079
daichi0812 <levi8164@icloud.com>
2023-11-18 16:22:21 +0900

    first commit

---
+++ src/Components/firebase.jsx
@@ -0,0 +1,19 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+const firebaseApp = firebase.initializeApp({
+    apiKey: "AIzaSyAEWvOQg8SscfIuhHUDkrQ_S8cxeGwqy0s",
+    authDomain: "chat-app-fa559.firebaseapp.com",
+    projectId: "chat-app-fa559",
+    storageBucket: "chat-app-fa559.appspot.com",
+    messagingSenderId: "337007651039",
+    appId: "1:337007651039:web:61f7b42cdf6d37e6eaa16a",
+    measurementId: "G-S5BRPM8GRC"
+})
+
+const db = firebaseApp.firestore();
+
+const auth = firebase.auth();
+
+export{db, auth};
